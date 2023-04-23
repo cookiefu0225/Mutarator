844b5d1d4a8fe970ef8309c689b43348ba733179
haowenmvp <panhaowen789@gmail>
2023-04-22 20:51:16 -0500

    fix voidmethodcall

--- src/main/java/edu/illinois/mutarator/returns/VoidMethodCall.java
+++ src/main/java/edu/illinois/mutarator/returns/VoidMethodCall.java
@@ -1,18 +1,41 @@
 package edu.illinois.mutarator.returns;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.UnaryExpr;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 public class VoidMethodCall extends VoidVisitorAdapter {
     @Override
-    public void visit(MethodCallExpr n, Object org) {
-        super.visit(n, org);
-
-        if(n.calculateResolvedType().toString().equals("voidType")){
-            n.remove();
+    public void visit(MethodDeclaration md, Object org) {
+        super.visit(md, org);
+        if(md.getType().toString().equals("void")){
+//            System.out.println(md.getName());
+            md.accept(new MethodCallVisitor(), null);
         }
 
 
+
+    }
+}
+
+class MethodCallVisitor extends ModifierVisitor<Void> {
+    @Override
+    public Visitable visit(MethodCallExpr n, Void arg) {
+        // Found a method call
+//        n.remove();
+//        System.out.println(n.getScope() + " - " + n.getName());
+//        System.out.println(n.getScope() + " - " + n.getName());
+//        System.out.println(n.getScope() + " - " + n.getName());
+        Node parentNode = n.getParentNode().get();
+        Node grandParentNode = parentNode.getParentNode().get();
+//        System.out.println(grandParentNode.remove(parentNode));
+
+        // Don't forget to call super, it may find more method calls inside the arguments of this method call, for example.
+//        super.visit(n, arg);
+        return super.visit(n, arg);
     }
 }
--- src/test/resources/sample/ToyProgramVoidMethodCall-Modified.java
+++ src/test/resources/sample/ToyProgramVoidMethodCall-Modified.java
@@ -1,10 +1,10 @@
 public class ToyProgramVoidMethodCall {
     public void someVoidMethod(int i) {
-        System.out.println(1);
     }
 
     public int foo() {
         int i = 5;
+        someVoidMethod(i);
         return i;
     }
 }
